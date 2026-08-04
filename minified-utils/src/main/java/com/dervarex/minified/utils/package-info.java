c4fcb1493444052e51e4a951c0e6ff2ccb1fa20d
dervarex <177766112+dervarex@users.noreply.github.com>
2026-08-04 21:17:21 +0200

    Add a notice that a specific part of minified-utils may not be as accessible as the rest

--- minified-utils/src/main/java/com/dervarex/minified/utils/package-info.java
+++ minified-utils/src/main/java/com/dervarex/minified/utils/package-info.java
@@ -1,9 +1,8 @@
 /**
  * Shared utility APIs used across the project.
- * May be used by the Launcher, as there's no internal-only code in here.
+ * May be used by the Launcher, as there's no internal-only code in here, except for some parts of {@link com.dervarex.minified.utils.version}, which mostly can be used too, but be careful with those.
  *
  * <p>Use {@code http}, {@code json}, {@code sha}, and {@code exceptions} from application code.
  * The {@code network} package is internal support code and should not be used directly.</p>
  */
 package com.dervarex.minified.utils;
-
--- minified-utils/src/main/java/com/dervarex/minified/utils/version/package-info.java
+++ minified-utils/src/main/java/com/dervarex/minified/utils/version/package-info.java
@@ -3,6 +3,8 @@
  *
  * <p>Use this package when you need to inspect versions, manifests, or metadata outside the
  * default launcher flow.</p>
+ *
+ * Attention: Some parts here aren't as accessible as the rest of this module, you for sure can use them, but be careful.
  */
 package com.dervarex.minified.utils.version;
 
