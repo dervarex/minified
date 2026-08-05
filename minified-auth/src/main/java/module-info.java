7f5ac579c21d3e936612c2dba9410947a0a7aade
dervarex <177766112+dervarex@users.noreply.github.com>
2026-08-05 14:45:26 +0200

    fix module files and gradle (java modules were a mistake)

--- minified-auth/src/main/java/module-info.java
+++ minified-auth/src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module minified.minified.auth.main {
     exports com.dervarex.minified.auth;
+
     requires com.google.gson;
-    requires httpclient;
-    requires MinecraftAuth;
     requires org.jetbrains.annotations;
+
     requires minified.minified.utils.main;
     requires minified.minified.events.main;
     requires minified.minified.java.main;
+
+    requires httpclient;
+    requires MinecraftAuth;
 }
--- minified-modrinth/src/main/java/module-info.java
+++ minified-modrinth/src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module minified.minified.modrinth.main {
     requires java.net.http;
+    requires minified.minified.utils.main;
 }
