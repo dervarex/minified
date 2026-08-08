6837ebf43ce079023abe0736cded6e5cb636d735
dervarex <177766112+dervarex@users.noreply.github.com>
2026-08-08 11:26:11 +0200

    correct Javadoc in VersionManifestClient

--- minified-utils/src/main/java/com/dervarex/minified/utils/version/VersionManifestClient.java
+++ minified-utils/src/main/java/com/dervarex/minified/utils/version/VersionManifestClient.java
@@ -16,7 +16,7 @@ public final class VersionManifestClient {
 
     /**
      * @return the Minecraft version manifest
-     * @throws HttpException if there is no working connection
+     * @throws HttpException when no connection could be established or any other Http Error occurs
      * @throws IOException if there is an error reading the response
      */
     public static JsonFile getManifest() throws HttpException, IOException {
@@ -26,8 +26,8 @@ public final class VersionManifestClient {
     /**
      * @return all Minecraft versions as an iterable of JSON values,
      * each containing the version's metadata (id, type, url, time, releaseTime)
-     * @throws HttpException
-     * @throws IOException
+     * @throws HttpException when no connection could be established or any other Http Error occurs
+     * @throws IOException if there is an error reading the response
      */
     public static Iterable<JsonValue> getVersions() throws HttpException, IOException {
         return getManifest().get("versions").asArray();
