c46f9747af0b483ff3270315d3fae35c6d337985
dervarex <177766112+dervarex@users.noreply.github.com>
2026-08-27 09:29:32 +0200

    use lombok for annotations and add checks if the fields we request exist

--- minified-worlds/src/main/java/com/dervarex/minified/worlds/world/level/Spawn.java
+++ minified-worlds/src/main/java/com/dervarex/minified/worlds/world/level/Spawn.java
@@ -1,42 +1,54 @@
 package com.dervarex.minified.worlds.world.level;
 
 import com.dervarex.minified.utils.nbt.tag.NbtCompound;
+import com.dervarex.minified.utils.nbt.tag.NbtIntArray;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jetbrains.annotations.Nullable;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class Spawn {
+    @Nullable
     private String dimension;
     private int pitch;
     private int yaw;
+    @Nullable
     private int[] pos;
-
-    public Spawn() {}
 
     public static Spawn fromNbt(NbtCompound spawnNbt) {
         Spawn spawn = new Spawn();
-        spawn.dimension = spawnNbt.getString("dimension");
-        spawn.pitch = spawnNbt.getInt("pitch");
-        spawn.yaw = spawnNbt.getInt("yaw");
-        spawn.pos = spawnNbt.getIntArray("pos").value();
+        if (spawnNbt.has("dimension")) {
+            spawn.dimension = spawnNbt.getString("dimension");
+        }
+        if (spawnNbt.has("pitch")) {
+            spawn.pitch = spawnNbt.getInt("pitch");
+        }
+        if (spawnNbt.has("yaw")) {
+            spawn.yaw = spawnNbt.getInt("yaw");
+        }
+        if (spawnNbt.has("pos")) {
+            spawnNbt.get("pos").ifPresent(tag -> {
+                if (tag instanceof NbtIntArray intArray) {
+                    spawn.pos = intArray.value();
+                }
+            });
+        }
         return spawn;
     }
 
     public NbtCompound toNbt() {
         NbtCompound nbt = new NbtCompound();
-        nbt.setString("dimension", dimension);
+        if (dimension != null) {
+            nbt.setString("dimension", dimension);
+        }
         nbt.setInt("pitch", pitch);
         nbt.setInt("yaw", yaw);
-        nbt.setIntArray("pos", pos);
+        if (pos != null) {
+            nbt.setIntArray("pos", pos);
+        }
         return nbt;
     }
-
-    public String getDimension() { return dimension; }
-    public void setDimension(String dimension) { this.dimension = dimension; }
-
-    public int getPitch() { return pitch; }
-    public void setPitch(int pitch) { this.pitch = pitch; }
-
-    public int getYaw() { return yaw; }
-    public void setYaw(int yaw) { this.yaw = yaw; }
-
-    public int[] getPos() { return pos; }
-    public void setPos(int[] pos) { this.pos = pos; }
 }
