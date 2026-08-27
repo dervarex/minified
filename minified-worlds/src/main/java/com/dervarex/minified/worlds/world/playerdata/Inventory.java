0424d059073979a86f7f764d95fbf4784ecbd1af
dervarex <177766112+dervarex@users.noreply.github.com>
2026-08-27 20:29:14 +0200

    add Inventory

---
+++ minified-worlds/src/main/java/com/dervarex/minified/worlds/world/playerdata/Inventory.java
@@ -0,0 +1,40 @@
+package com.dervarex.minified.worlds.world.playerdata;
+
+import com.dervarex.minified.utils.nbt.tag.NbtCompound;
+import com.dervarex.minified.utils.nbt.tag.NbtList;
+
+public class Inventory {
+    InventoryItem[] items;
+
+    public static Inventory fromNbtList(NbtList nbt) {
+        Inventory inventory = new Inventory();
+        inventory.items = new InventoryItem[nbt.size()];
+        for (int i = 0; i < nbt.size(); ++i) {
+            NbtCompound entry = (NbtCompound) nbt.elements().get(i);
+            InventoryItem item = new InventoryItem();
+            item.id = entry.getString("id");
+            item.count = entry.getByte("count");
+            item.slot = entry.getByte("Slot");
+            inventory.items[i] = item;
+        }
+        return inventory;
+    }
+
+    public NbtList toNbtList() {
+        NbtList list = new NbtList((byte) 10); // 10 = compound tag id
+        for (InventoryItem item : items) {
+            NbtCompound entry = new NbtCompound();
+            entry.setString("id", item.id);
+            entry.setByte("count", item.count);
+            entry.setByte("Slot", item.slot);
+            list.add(entry);
+        }
+        return list;
+    }
+
+    public static class InventoryItem {
+        String id;
+        byte count;
+        byte slot;
+    }
+}
