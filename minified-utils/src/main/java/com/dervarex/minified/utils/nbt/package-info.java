8b35efc0f028622b952c1404745b3aeacfc90b08
dervarex <177766112+dervarex@users.noreply.github.com>
2026-08-30 19:36:59 +0200

    add LZ4 notice to package info

--- minified-utils/src/main/java/com/dervarex/minified/utils/nbt/package-info.java
+++ minified-utils/src/main/java/com/dervarex/minified/utils/nbt/package-info.java
@@ -1,4 +1,10 @@
 /**
- * NBT(Named Binary Tag) parsing utility.
+ * <pre>
+ * NBT(Named Binary Tag) and MCA(Minecraft Anvil Files) parsing utility.
+ *
+ * Note: We do not support LZ4 Compressed Chunk files, since it's just
+ * a flag in the server.properties file (building an LZ4 Parser is a
+ * lot of effort, and I do not want to add a library just for this)
+ * </pre>
  */
 package com.dervarex.minified.utils.nbt;
