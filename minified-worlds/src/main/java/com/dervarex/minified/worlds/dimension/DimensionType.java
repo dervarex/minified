25a1579ada1bf3b0c0a2a6b1fad0682b1f956cac
dervarex <177766112+dervarex@users.noreply.github.com>
2026-09-03 10:08:54 +0200

    add DimensionType.fromFolder

--- minified-worlds/src/main/java/com/dervarex/minified/worlds/dimension/DimensionType.java
+++ minified-worlds/src/main/java/com/dervarex/minified/worlds/dimension/DimensionType.java
@@ -1,11 +1,28 @@
 package com.dervarex.minified.worlds.dimension;
 
+import java.io.File;
+
 public enum DimensionType {
     Overworld("overworld"),
     Nether("the_nether"),
     End("the_end");
+
     protected final String dimensionName;
+
     DimensionType(String dimensionName) {
         this.dimensionName = dimensionName;
+    }
+
+    /**
+     * @param worldFolder world folder
+     * @return {@link DimensionType} based on the folder name
+     */
+    public static DimensionType fromFolder(File worldFolder) {
+        for (DimensionType type : values()) {
+            if (type.dimensionName.equalsIgnoreCase(worldFolder.getName())) {
+                return type;
+            }
+        }
+        return null;
     }
 }
