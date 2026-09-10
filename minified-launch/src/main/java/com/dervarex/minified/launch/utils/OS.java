61f05fb10d59c9ce34b4fd52c01f32c5a20859bd
dervarex <177766112+dervarex@users.noreply.github.com>
2026-09-10 11:06:58 +0200

    add os enum and getOS method using that enum

---
+++ minified-launch/src/main/java/com/dervarex/minified/launch/utils/OS.java
@@ -0,0 +1,8 @@
+package com.dervarex.minified.launch.utils;
+
+public enum OS {
+    WINDOWS,
+    LINUX,
+    MACOS,
+    UNKNOWN
+}
--- minified-launch/src/main/java/com/dervarex/minified/launch/utils/OSUtil.java
+++ minified-launch/src/main/java/com/dervarex/minified/launch/utils/OSUtil.java
@@ -19,4 +19,15 @@ public class OSUtil {
 
         return "unknown";
     }
+    public static OS getOS() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            return OS.WINDOWS;
+        } else if (os.contains("mac") || os.contains("darwin")) {
+            return OS.MACOS;
+        } else if (os.contains("nix") || os.contains("nux")) {
+            return OS.LINUX;
+        }
+        return OS.UNKNOWN;
+    }
 }
