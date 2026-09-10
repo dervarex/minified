405b752872043f2be222bce298ede948a8916c90
dervarex <177766112+dervarex@users.noreply.github.com>
2026-09-10 14:14:40 +0200

    Add custom string names to OS enum

--- minified-utils/src/main/java/com/dervarex/minified/utils/os/OS.java
+++ minified-utils/src/main/java/com/dervarex/minified/utils/os/OS.java
@@ -1,8 +1,23 @@
 package com.dervarex.minified.utils.os;
 
 public enum OS {
-    WINDOWS,
-    LINUX,
-    MACOS,
-    UNKNOWN
+    WINDOWS("windows"),
+    LINUX("linux"),
+    MACOS("osx"),
+    UNKNOWN("unknown");
+
+    private final String name;
+
+    OS(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
